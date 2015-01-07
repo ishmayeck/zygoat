6c00191ef02c87380b9c7e3e6c024bccfcb23722
Tyson Cleary <tyscorp@gmail.com>
2015-01-07 13:59:33 +1100

    add support for multiple irc libraries

--- index.js
+++ index.js
@@ -2,60 +2,58 @@ var Router = require('./lib/router.js');
 var Request = require('./lib/request.js');
 var Response = require('./lib/response.js');
 
-var App = function() {
+var App = function (lib) {
+    if (!lib) lib = 'node-irc';
+    this.lib = require('./lib/irc/' + lib.toLowerCase());
     this.router = new Router();
 };
 
 App.prototype.middleware = [];
 
+App.prototype.colors = require('irc-colors');
+
 /**
  * Add a middleware to the stack
  * @param fn {Function} Middleware function
  */
-App.prototype.use = function(fn) {
+App.prototype.use = function (fn) {
     console.log('using middleware: ', arguments);
     this.middleware.push(fn);
 };
 
-App.prototype.run = function(client) {
+App.prototype.run = function (client) {
     var self = this;
-    /**
-     * Create a req/res pair and dispatch it
-     * Should these be called something other than req/res maybe? eh?
-     */
-    client.addListener('error', function(e) {
-        console.log('ERROOORRRRR', e);
-    });
+
     this.use(function() {
         self.router.handle.apply(self.router, arguments)
     });
-    client.on('raw', function(message) {
+
+    this.lib.runner(client, function (message) {
         var req = new Request(client, message);
         var res = new Response(client, message);
+
         App.prototype.dispatch(req, res);
     });
 };
 
-App.prototype.dispatch = function(req, res, index) {
-    if(!index) index = 0;
-    if(index > this.middleware.length - 1) return;
-    console.log('executing middleware #', index, 'for message', req.rawCommand);
+App.prototype.dispatch = function (req, res, index) {
+    if (!index) index = 0;
+    if (index > this.middleware.length - 1) return;
 
     var self = this;
-    this.middleware[index](req, res, function(err) {
-        if(!err) self.dispatch(req, res, index + 1);
+    this.middleware[index](req, res, function (err) {
+        if (!err) self.dispatch(req, res, index + 1);
     });
 };
 
-App.prototype.cmd = function() {
+App.prototype.cmd = function () {
     this.router.cmd.apply(this.router, arguments);
 };
 
-App.prototype.msg = function() {
+App.prototype.msg = function () {
     this.router.msg.apply(this.router, arguments);
 };
 
-module.exports = function() {
-    return new App();
+module.exports = function (lib) {
+    return new App(lib);
 };
-
